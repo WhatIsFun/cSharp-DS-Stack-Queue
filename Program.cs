c406674e3a71f29fff8c896349d03bc9b2cff0e0
WhatIsFun <mohammed110051@gmail.com>
2023-07-11 11:20:49 +0400

    Stack Push, Pop, Peek, and Display

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace cSharp_DSStackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack S = new Stack(5);
+
+            S.StackAdd(1);
+            S.StackAdd(54);
+            S.StackAdd(7);
+            S.StackAdd(654);
+            S.StackAdd(640);
+
+            Console.WriteLine("Items are : ");
+            S.Display();
+            S.StackPeek();
+
+            S.StackRemove();
+            S.StackRemove();
+            S.StackRemove();
+
+            S.Display();
+            S.StackPeek();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace cSharp_DSStackQueue
+{
+    internal class Stack
+    {
+        private int[] S;
+        private int top;
+        private int max;
+        public Stack(int size)
+        {
+            S = new int[size];
+            top = -1;
+            max = size;
+        }
+
+        public void StackAdd(int n)
+        {
+            if (top == max - 1)
+            {
+                Console.WriteLine("Stack is Full");
+                return;
+            }
+            else
+            {
+                S[++top] = n;
+            }
+        }
+
+        public int StackRemove()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("Poped element is: " + S[top]);
+                return S[top--];
+            }
+        }
+
+        public void StackPeek()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("The stack is empty");
+            }
+            else
+            {
+                Console.WriteLine("The top element is: " + S[top]);
+            }
+
+        }
+        public void Display()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + S[i]);
+                }
+            }
+        }
+
+        
+    }
+}
