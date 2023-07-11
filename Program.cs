ddc92f96e7784d2122677a0abf215c51fbbb94fc
WhatIsFun <mohammed110051@gmail.com>
2023-07-11 14:01:24 +0400

    Queue Task

--- Program.cs
+++ Program.cs
@@ -4,24 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Stack S = new Stack(5);
+            //Stack S = new Stack(5);
 
-            S.StackAdd(1);
-            S.StackAdd(54);
-            S.StackAdd(7);
-            S.StackAdd(654);
-            S.StackAdd(640);
+            //S.StackAdd(1);
+            //S.StackAdd(54);
+            //S.StackAdd(7);
+            //S.StackAdd(654);
+            //S.StackAdd(640);
 
-            Console.WriteLine("Items are : ");
-            S.Display();
-            S.StackPeek();
+            //Console.WriteLine("Items are : ");
+            //S.Display();
+            //S.StackPeek();
 
-            S.StackRemove();
-            S.StackRemove();
-            S.StackRemove();
+            //S.StackRemove();
+            //S.StackRemove();
+            //S.StackRemove();
 
-            S.Display();
-            S.StackPeek();
+            //S.Display();
+            //S.StackPeek();
+
+
+
+            Queue Q = new Queue(5);
+            Q.QueueAdd(10);
+            Q.QueueAdd(20);
+            Q.QueueAdd(30);
+            Q.QueueAdd(40);
+            Q.QueueAdd(50);
+            Q.Display();
+
+            Q.QueueRemove();
+            Q.Display();
+
+            Q.QueuePeek();
+            Q.QueueRemove();
+            Q.Display();
+
+
+
         }
     }
 }
---
+++ Queue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_DSStackQueue
+{
+    internal class Queue
+    {
+        private int[] Q;
+        private int rear;
+        private int front;
+        //private int max;
+        public int size;
+        public Queue(int size) 
+        {
+            Q = new int[size];
+            this.size = size;
+            rear = 0;
+            front = 0;
+        }
+        public void QueueAdd(int n)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The Queue is full !!");
+                return;
+            }
+            Q[rear] = n;
+            rear++;
+            return;
+        }
+        public void QueueRemove()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Dequeue element is: " + Q[front]);
+                for (int i = 0; i < rear - 1; i++)
+                {
+                    Q[i] = Q[i + 1];
+                }
+                if (rear < size)
+                    Q[rear] = 0;
+                rear--;
+            }
+            return;
+        }
+        public void QueuePeek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The stack is empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("The front element is: " + Q[front]);
+            }
+        }
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The Queue is Empty");
+                return;
+            }
+            Console.WriteLine("Queue :");
+            for (int i = front; i < rear; i++)
+            {
+                Console.WriteLine("Item[" + (i + 1) + "]: " + Q[i]);
+            }
+            return;
+        }
+        private bool IsEmpty()
+        {
+            return rear == front;
+        }
+        private bool IsFull()
+        {
+            return rear == size;
+        }
+    }
+}
